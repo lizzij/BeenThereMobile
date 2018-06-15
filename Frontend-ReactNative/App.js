917965ff57257a7163e6496b70aec4e43a43ff06
韩笑尘 <hanx2021@berkeley.edu>
2018-06-15 11:16:20 -0400

    initial commit

---
+++ Frontend-ReactNative/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ScrollView, Alert, Button, StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import { List, ListItem, SearchBar, Avatar } from 'react-native-elements';
+// import { FriendListGenerator } from './screens/FriendListGenerator'
+// import { MessageListGenerator } from './screens/MessageListGenerator'
+import { RegistrationPage } from './screens/RegistrationPage'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <RegistrationPage/>
+    );
+  }
+}
