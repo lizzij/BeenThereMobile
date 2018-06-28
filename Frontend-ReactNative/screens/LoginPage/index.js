541f9ff6f6aa3cd82897cfd0dea717a4feba8a1f
韩笑尘 <hanx2021@berkeley.edu>
2018-06-28 16:12:50 +0800

    fixed a bug on button press response, style changes and added documentation

--- Frontend-ReactNative/screens/LoginPage/index.js
+++ Frontend-ReactNative/screens/LoginPage/index.js
@@ -3,22 +3,22 @@ import axios from 'axios';
 import { Button, Text, Image, ImageBackground, Dimensions, TextInput, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 const serverUrl = 'http://beenthere.us-east-2.elasticbeanstalk.com';
-const http = axios.create( {
+const http = axios.create({
   baseURL: serverUrl,
-} );
+});
 
-const {width, height} = Dimensions.get( 'window' );
+const {width, height} = Dimensions.get('window');
 
-const background = require( './login_bg.png' );
-const logo = require( './login_logo.png' );
-const title = require( './login_title.png' );
-const emailIcon = require( './login_email.png' );
-const passwordIcon = require( './login_password.png' );
-const button = require( './login_button.png' );
+const background = require('./login_bg.png');
+const logo = require('./login_logo.png');
+const title = require('./login_title.png');
+const emailIcon = require('./login_email.png');
+const passwordIcon = require('./login_password.png');
+const loginButton = require('./login_button.png');
 
 export class LoginPage extends Component {
-  constructor( props ) {
-    super( props );
+  constructor(props) {
+    super(props);
 
     this.state = {
       status: '',
@@ -28,96 +28,94 @@ export class LoginPage extends Component {
   }
 
   onLogin() {
-    const {userid, password} = this.state;
-    http.post( '/login', {
-      userid: this.state.userid,
-      password: this.state.password,
-    } )
-      .then( function ( response ) {
-        console.log( response.data[ 'status' ] );
-      } )
-      .catch( function ( error ) {
-        console.log( error );
-      } );
+    console.log('sending request');
+    var self = this;
+    http.post('/login', {
+      userid: self.state.userid,
+      password: self.state.password,
+    })
+    .then(function (response) {
+      self.setState({status: response.data['status']});
+      console.log(self.state.status);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
   render() {
     return (
     <View style={ styles.container }>
+      // background image
       <ImageBackground source={ background }
                        style={ styles.background }
                        resizeMode="stretch">
-        <View style={ styles.logoWrap }>
-          <Image source={ logo }
-                 style={ styles.logo }
-                 resizeMode="contain" />
-        </View>
-        <View style={ styles.titleWrap }>
-          <Image source={ title }
-                 style={ styles.title }
-                 resizeMode="contain" />
-        </View>
-        <View style={ styles.wrapper }>
-          <View style={ styles.inputWrap }>
-            <View style={ styles.iconWrap }>
-              <Image source={ emailIcon }
-                     style={ styles.emailIcon }
-                     resizeMode="contain" />
-            </View>
-            <TextInput onChangeText={ (userid) => this.setState( {
-                                        userid
-                                      } ) }
-                       placeholder={ '邮箱' }
+      // app logo
+      <View style={ styles.logoWrap }>
+        <Image source={ logo }
+               style={ styles.logo }
+               resizeMode="contain" />
+      </View>
+
+      // app title
+      <View style={ styles.titleWrap }>
+        <Image source={ title }
+               style={ styles.title }
+               resizeMode="contain" />
+      </View>
+
+      // user input fields
+      <View style={ styles.wrapper }>
+        // --email input
+        <View style={ styles.inputWrap }>
+          <View style={ styles.iconWrap }>
+            <Image source={ emailIcon }
+                   style={ styles.emailIcon }
+                   resizeMode="contain" />
+          </View>
+          <TextInput onChangeText={(userid) => this.setState({userid})}
+                       placeholder={'邮箱'}
                        placeholderTextColor="white"
                        style={ styles.input } />
+        </View>
+        <View style={ styles.seperator } />
+        // --password input
+        <View style={ styles.inputWrap }>
+          <View style={ styles.iconWrap }>
+            <Image source={ passwordIcon }
+                   style={ styles.passwordIcon }
+                   resizeMode="contain" />
           </View>
-          <View 
-            style={ styles.seperator }
-          />
-          <View style={ styles.inputWrap }>
-            <View style={ styles.iconWrap }>
-              <Image source={ passwordIcon }
-                     style={ styles.passwordIcon }
-                     resizeMode="contain" />
-            </View>
-            <TextInput onChangeText={ (password) => this.setState( {
-                                        password
-                                      } ) }
-                       placeholder={ '密码' }
-                       placeholderTextColor="white"
-                       secureTextEntry={ true }         
-                       style={ styles.input } />
+          <TextInput onChangeText={(password) => this.setState({password})}
+                     placeholder={'密码'}
+                     placeholderTextColor="white"
+                     secureTextEntry={true}
+                     style={ styles.input } />
+        </View>
+        // --login button
+        <TouchableOpacity activeOpacity={ 0.5 } onPress={(this.onLogin.bind(this))}>
+          <View style={ styles.button }>
+            <Image source={ loginButton }
+                   style={ styles.button }
+                   resizeMode="contain" />
           </View>
-          <TouchableOpacity activeOpacity={ 0.5 }>
-            <View style={ styles.button }>
-              <Image source={ button }
-                     style={ styles.button }             
-                     onPress={this.onLogin.bind(this)}
-                     resizeMode="contain" />
-            </View>
+        </TouchableOpacity>
+      </View>
+
+      // other options
+      <View style={ styles.container }>
+        <View style={ styles.forgotPasswordWrap }>
+          <TouchableOpacity activeOpacity={ .5 }>
+            <View><Text style={ styles.forgotPasswordText }>忘记密码</Text></View>
           </TouchableOpacity>
         </View>
-        <View style={ styles.container }>
-          <View style={ styles.forgotPasswordWrap }>
-            <TouchableOpacity activeOpacity={ .5 }>
-              <View>
-                <Text style={ styles.forgotPasswordText }>
-                  忘记密码
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          <View style={ styles.signupWrap }>
-            <TouchableOpacity activeOpacity={ .5 }>
-              <View>
-                <Text style={ styles.signupLinkText }>
-                  注册新账号
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
+
+        <View style={ styles.signupWrap }>
+          <TouchableOpacity activeOpacity={ .5 }>
+            <View><Text style={ styles.signupLinkText }>注册新账号</Text></View>
+          </TouchableOpacity>
         </View>
-      </ImageBackground>
+      </View></ImageBackground>
     </View>
     );
   }
@@ -141,7 +139,7 @@ const styles = StyleSheet.create( {
     shadowColor: 'rgba(0, 0, 0, 0.25)',
     shadowOffset: {
       width: 0,
-      height: 3                   
+      height: 3
     },
     shadowRadius: 11,
     shadowOpacity: 1
@@ -172,7 +170,7 @@ const styles = StyleSheet.create( {
     height: 22,
   },
   seperator: {
-    borderBottomWidth: 1,              
+    borderBottomWidth: 1,
     borderBottomColor: '#FFF'
   },
   iconWrap: {
@@ -197,7 +195,7 @@ const styles = StyleSheet.create( {
     letterSpacing: 0,
     flexDirection: 'column',
     color: '#FFF',
-    flex: 1, 
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     marginLeft: 147,                 // text not shifting to left (reference)
@@ -244,4 +242,4 @@ const styles = StyleSheet.create( {
     color: '#FFF',
     alignItems: 'center',
   }
-} );
+});
