c81a488ccccea432dbf0664f157567ca7fc28398
韩笑尘 <hanx2021@berkeley.edu>
2018-06-30 19:41:22 +0800

    fixed a bug where chinese entries appear scrambled on terminal

--- WebApp-Flask/READ_ME_MySQL.sql
+++ WebApp-Flask/READ_ME_MySQL.sql
@@ -10,11 +10,17 @@ nc -zv beenthere.c96fpbfeupce.us-east-2.rds.amazonaws.com 3306
 # invoking mySQL:
 mysql -h beenthere.c96fpbfeupce.us-east-2.rds.amazonaws.com -P 3306 -u hanx0621 -p
 
+# database creation:
+CREATE DATABASE beenthereWeb
+   DEFAULT CHARACTER SET utf8
+   DEFAULT COLLATE utf8_general_ci;
+
 # initial commands:
+mysql> SET NAMES utf8;
 mysql> USE beenthereWeb;
 mysql> SELECT DATABASE();
 mysql> SHOW TABLES;
 
 # primary table creation:
 CREATE TABLE articles (articleid INTEGER PRIMARY KEY AUTO_INCREMENT, nickname TEXT, 
-	title TEXT NOT NULL, content TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);
+    title TEXT NOT NULL, content TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);
