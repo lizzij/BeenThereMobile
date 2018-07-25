050ec2ba17505e92d2e3b1d933e02c51f919669a
eliza <elizazijinhuang@gmail.com>
2018-07-25 02:26:55 -0700

    using ZanUI and WeUI

--- Frontend-WeApp/pages/publish/publish.js
+++ Frontend-WeApp/pages/publish/publish.js
@@ -1,78 +1,66 @@
+// pages/setting/setting.js
 Page({
-    data: {
-        item: {
-            key: "",
-            value: {
-                title: "",
-                content: ""
-            },
-            create_time: "",
-            update_time: "",
-            state: 1
-        },
-        isNew: false,
-        focus: false
-    },
-    onLoad: function(options) {
-        //
-    },
 
-    /**
-     * 页面渲染事件
-     */
-    onShow: function() {
-        var item = this.data.item;
-        item.key = hotapp.genPrimaryKey('item');
-        this.setData({
-            item: item
-        });
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
 
-    /**
-     * 保存数据事件
-     */
-    onSubmit: function(event) {
-        var item = this.data.item;
-        item.value.title = event.detail.value.title;
-        item.value.content = event.detail.value.content;
-        this.setData({
-            item: item
-        });
-        this.saveData();
-    },
-    onFocus:function(e){
-        this.setData({
-            focus: true
-        });
-    },
-    /**
-     * 请求服务器保存数据
-     */
-    saveData: function() {
-        var item = this.data.item;
-        var now = Date.parse(new Date()) / 1000;
-        item.update_time = now;
-        item.create_time = now;
-        this.setData({
-            item: item
-        });
-        api.store(this.data.item, function(res) {
-            if (res) {
-                wx.showToast({
-                    title: "保存成功",
-                    success:function(){
-                        // 返回首页
-                        setTimeout(function(){
-                            wx.hideToast();
-                            wx.navigateBack();
-                        },1000)
-                    }
-                });
-            } else {
-                wx.showToast({
-                    title: "保存失败"
-                });
-            }
-        });
-    }
-});
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- Frontend-WeApp/pages/setting/setting.js
+++ Frontend-WeApp/pages/setting/setting.js
@@ -1,66 +1,83 @@
 // pages/setting/setting.js
+const config = require('./config');
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    config,
+    value: 'test',
+    textareaValue: 'test textarea',
+    area: ['省份', '北京市', '天津市', '河北省', '山西省', '内蒙古自治区', '辽宁省', '吉林省', '黑龙江省', '上海市', '江苏省', '浙江省', '安徽省', '福建省', '江西省', '山东省', '河南省', '湖北省', '湖南省', '广东省', '广西壮族自治区', '海南省', '重庆市', '四川省', '贵州省', '云南省', '西藏自治区', '陕西省', '甘肃省', '青海省', '宁夏回族自治区', '新疆维吾尔自治区', '台湾省', '香港特别行政区', '澳门特别行政区'],
+    areaIndex: 0,
+    // picker-view 示例配置
+    pickerViewConfig: {
+      show: false,
+      value: [0, 0],
+      year: [2016, 2017, 2018],
+      sex: ['男', '女']
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onAreaChange(e) {
+    this.setData({
+      areaIndex: e.detail.value
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  handleZanFieldChange(e) {
+    const { detail } = e;
 
+    console.log('[zan:field:change]', detail);
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  handleZanFieldFocus(e) {
+    const { detail } = e;
 
+    console.log('[zan:field:focus]', detail);
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  handleZanFieldBlur(e) {
+    const { detail } = e;
 
+    console.log('[zan:field:blur]', detail);
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  clearInput() {
+    this.setData({
+      value: ''
+    });
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  clearTextarea() {
+    this.setData({
+      textareaValue: ''
+    });
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  formSubmit(event) {
+    console.log('[zan:field:submit]', event.detail.value);
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  formReset(event) {
+    console.log('[zan:field:reset]', event);
+  },
 
+  /* piker-view 示例相关函数 */
+  handleDateFieldClick() {
+    this.setData({
+      'pickerViewConfig.show': true
+    });
+  },
+
+  handlePopupDateChange(e) {
+    this.setData({
+      'pickerViewConfig.value': e.detail.value
+    });
+  },
+
+  hideDatePopup() {
+    this.setData({
+      'pickerViewConfig.show': false
+    });
   }
-})
+
+});
